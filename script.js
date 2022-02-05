1c55276c2f07ffdc6f4e5e4eb37288ef398c2f0d
moanasfiroz <87444626+moanasfiroz@users.noreply.github.com>
2022-02-05 21:56:25 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navList = document.querySelector('.nav-list');
-
-hamburger.addEventListener('click', () => {
-    navList.classList.toggle('show');
-});
